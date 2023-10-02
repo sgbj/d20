62e201686f80d96751a16faf484cc29bb4861831
sgbj <scott.batary@gmail.com>
2023-10-01 22:46:13 -0500

    updated models

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import {
   Engine,
   Scene,
   Vector3,
-  HemisphericLight,
   Mesh,
   MeshBuilder,
   HavokPlugin,
@@ -16,6 +15,9 @@ import {
   Camera,
   Color4,
   SceneLoader,
+  PhysicsViewer,
+  HemisphericLight,
+  ScenePerformancePriority,
 } from "@babylonjs/core";
 import HavokPhysics from "@babylonjs/havok";
 
@@ -26,12 +28,14 @@ async function main() {
   const engine = new Engine(canvas, true);
   const scene = new Scene(engine);
   scene.clearColor = new Color4(0, 0, 0, 0);
-
-  const camera = new FreeCamera("camera", new Vector3(0, 20, 0), scene);
-  camera.setTarget(Vector3.Zero());
-  camera.mode = Camera.ORTHOGRAPHIC_CAMERA;
+  scene.performancePriority = ScenePerformancePriority.Aggressive;
 
   const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+  scene.createDefaultEnvironment({ createGround: false, createSkybox: false });
+
+  const camera = new FreeCamera("camera", new Vector3(0, 10, 0), scene);
+  camera.setTarget(Vector3.Zero());
+  camera.mode = Camera.ORTHOGRAPHIC_CAMERA;
 
   const havokInstance = await HavokPhysics();
   const havokPlugin = new HavokPlugin(true, havokInstance);
@@ -53,23 +57,50 @@ async function main() {
     scene
   );
 
-  await SceneLoader.AppendAsync("/", "scene.gltf");
+  async function loadDie(name: string) {
+    const res = await SceneLoader.ImportMeshAsync("", "/", name);
+    const newMesh = Mesh.MergeMeshes(
+      res.meshes.slice(1) as Mesh[],
+      true,
+      true,
+      undefined,
+      true,
+      true
+    )!;
+    newMesh?.scaling.scaleInPlace(0.04);
+    const d20Aggregate = new PhysicsAggregate(
+      newMesh,
+      PhysicsShapeType.CONVEX_HULL,
+      { mass: 1, restitution: 0.1, friction: 0.7 },
+      scene
+    );
 
-  scene.meshes[2].scaling.scaleInPlace(30);
+    d20Aggregate.body.setAngularVelocity(new Vector3(rng(), rng(), rng()));
 
-  const d20Aggregate = new PhysicsAggregate(
-    scene.meshes[2],
-    PhysicsShapeType.MESH,
-    { mass: 1, restitution: 0.3, friction: 0.75 },
-    scene
-  );
+    d20Aggregate.body.applyImpulse(
+      new Vector3(rng(), rng(), rng()),
+      new Vector3(rng(), rng(), rng())
+    );
+  }
 
-  const rng = () => (10 + Math.random() * 20) * (Math.random() > 0.5 ? 1 : -1);
-  d20Aggregate.body.setAngularVelocity(new Vector3(rng(), rng(), rng()));
+  function rng() {
+    return (25 + Math.random() * 25) * (Math.random() > 0.5 ? 1 : -1);
+  }
+
+  await loadDie("D4.glb");
+  await loadDie("D6.glb");
+  await loadDie("D8.glb");
+  await loadDie("D10.glb");
+  await loadDie("D12.glb");
+  await loadDie("D20.glb");
+
+  const physicsViewer = new PhysicsViewer();
+  //physicsViewer.showBody(d20Aggregate.body);
 
   engine.runRenderLoop(() => {
     scene.render();
   });
+  console.log(scene.meshes);
 
   window.addEventListener("resize", resize);
 
@@ -77,15 +108,15 @@ async function main() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    const orthoWidthValue = canvas.width / 125;
-    const orthoHeightValue = canvas.height / 125;
+    const orthoWidthValue = canvas.width / 100;
+    const orthoHeightValue = canvas.height / 100;
 
     camera.orthoBottom = -orthoHeightValue;
     camera.orthoTop = orthoHeightValue;
     camera.orthoLeft = -orthoWidthValue;
     camera.orthoRight = orthoWidthValue;
 
-    box.scaling = new Vector3(orthoWidthValue * 2, 1, orthoHeightValue * 2);
+    box.scaling = new Vector3(orthoWidthValue * 2, 2, orthoHeightValue * 2);
 
     engine.resize();
   }
